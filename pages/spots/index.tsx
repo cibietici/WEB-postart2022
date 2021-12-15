fe51633600886891cbe3b34340a21c389fe746ba
cibietici <carloalberto.burato@gmail.com>
2021-12-15 15:20:33 +0100

    Change style

--- pages/spots/index.tsx
+++ pages/spots/index.tsx
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import React, { SetStateAction } from 'react'
-import styles from '../../styles/Letters.module.css'
+import styles from '../../styles/Spots.module.css'
+import { spots, spotsEl } from '../../utils/utils'
 
 
 export default function Home() {
@@ -14,9 +15,22 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <div className={styles.letterBox}>
-          A
-        </div>
+        {
+          spotsEl.map((spot) =>(
+            <div key={spot.id} 
+              className={styles.spot + ' ' + styles.spotanimationup}
+              style={
+                {
+                  backgroundColor: spot.color,
+                  width: `${spot.width}px`,
+                  height: `${spot.width}px`,
+                  top: `${spot.posY}%`,
+                  left: `${spot.posX}%`
+                }
+                }>
+            </div>
+          ))
+        }
       </main>
     </div>
   )
--- utils/utils.tsx
+++ utils/utils.tsx
@@ -1,3 +1,41 @@
-export function first() {
-    return true
+export function spots(el) {
+
+    const radius = '50%'
+    const where = document.getElementById(el)
+
+    console.log(where)
+
+    const spot = document.createElement('div')
+    spot.style.backgroundColor = 'red'
+    spot.style.borderRadius = radius
+    where.append(spot)
+
+    where.innerHTML = 'a'
 }
+
+export const spotsEl = [
+    {
+        id: 1,
+        color: 'red',
+        width: 250,
+        posX: 30,
+        posY: 40,
+        animation: 'up'
+    },
+    {
+        id: 2,
+        color: 'red',
+        width: 150,
+        posX: 60,
+        posY: 80,
+        animation: 'down'
+    },
+    {
+        id: 2,
+        color: 'red',
+        width: 300,
+        posX: 30,
+        posY: 30,
+        animation: 'up'
+    }
+]
